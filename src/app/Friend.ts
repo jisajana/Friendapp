e516bf89809075b4e063e8917c4c5be862e40386
Sajana <jisajana@gmail.com>
2021-11-13 16:14:44 -0500

    Friend app first commit

---
+++ src/app/Friend.ts
@@ -0,0 +1,5 @@
+export class Friend {
+    friendId: number | undefined; //Spelling mismatch wont fetch data from java pojo
+    friendName: string | undefined;
+}
+//above two fields must match with java pojo fields
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FriendComponent } from './friend/friend.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
